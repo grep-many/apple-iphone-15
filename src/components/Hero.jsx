a9a80f2d0d51713bd37181310a9946c111e1610a
Manish <manishkumar9540898@gmail.com>
2025-12-06 17:05:34 +0530

    feat(section/hero):implemented base hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,68 @@
-const Hero = () => <div>Hero</div>;
+import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../assets";
+
+const Hero = () => {
+  const sourceRef = React.useRef(null);
+
+  const handleResize = () => {
+    const isMobile = window.innerWidth < 768;
+    const desiredSrc = isMobile ? smallHeroVideo : heroVideo;
+    if (!sourceRef?.current) return;
+    if (sourceRef.current.src.endsWith(desiredSrc)) return;
+    sourceRef.current.src = desiredSrc;
+    sourceRef.current.parentElement.load();
+  };
+
+  useGSAP(() => {
+    const isMobile = window.innerWidth < 768;
+    gsap.to("#hero", {
+      opacity: 1,
+      duration: 1,
+      delay: 0.5,
+    });
+    gsap.to("#cta", {
+      opacity: 1,
+      y: -50,
+      duration:1,
+      delay: isMobile?2.4:0.8,
+    });
+  }, [heroVideo, smallHeroVideo]);
+
+  React.useEffect(() => {
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      if (sourceRef?.current) sourceRef.current = null;
+    };
+  }, [heroVideo, smallHeroVideo]);
+
+  return (
+    <section className="nav-height relative">
+      <div className="flex-center h-5/6 flex-col">
+        <p className="hero-title" id="hero">
+          iPhone 15 Pro
+        </p>
+        <div className="w-9/12 md:w-10/12">
+          <video autoPlay muted playsInline className="pointer-events-none">
+            <source
+              ref={sourceRef}
+              src={window.innerWidth < 768 ? smallHeroVideo : heroVideo}
+              type="video/mp4"
+            />
+          </video>
+        </div>
+      </div>
+      <div id="cta" className="flex translate-y-20 flex-col items-center opacity-0">
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="text-xl font-normal">From 1199/month or 1999</p>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import { appleImg, bagImg, searchImg } from "@/assets";
 import { navLists } from "@/constants";
 
 const Navbar = () => (
-  <header className="flex items-center justify-between p-5 sm:px-10">
+  <header className="screen-max-width flex items-center justify-between p-5 sm:px-10">
     <img src={appleImg} alt="Apple" className="cursor-pointer" width={14} height={18} />
     <nav className="text-gray flex flex-1 justify-center gap-10 text-sm max-sm:hidden">
       {navLists.map((nav, i) => (
         <a key={i} className="group relative cursor-pointer transition-colors hover:text-white">
           {nav}
-          <span className="absolute bottom-0.5 group-hover:-bottom-px left-1/2 h-0.5 w-0 bg-white transition-[width_transform] group-hover:left-[10%] group-hover:w-[80%] motion-safe:duration-300" />
+          <span className="absolute bottom-0.5 left-1/2 h-0.5 w-0 bg-white transition-[width_transform] group-hover:-bottom-px group-hover:left-[10%] group-hover:w-[80%] motion-safe:duration-300" />
         </a>
       ))}
     </nav>
