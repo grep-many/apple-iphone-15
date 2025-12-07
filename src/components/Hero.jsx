e5f1d93fcd3fc1830837d87e0d15cf36a7de441d
Manish <manishkumar9540898@gmail.com>
2025-12-07 19:56:27 +0530

    chore(component/caraousel):add video carousel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,15 +4,33 @@ import { useGSAP } from "@gsap/react";
 import { heroVideo, smallHeroVideo } from "../assets";
 
 const Hero = () => {
-  const sourceRef = React.useRef(null);
+  const videoRef = React.useRef({
+    control: null,
+    source: null,
+  });
 
+  const loopEnd = () => {
+    const video = videoRef.current.control;
+    if (!video) return;
+
+    const loopStart = video.duration - 0.9  ;
+    if (video.currentTime >= video.duration) {
+      video.currentTime = loopStart;
+      video.playbackRate =0.5;
+      video.play();
+    }
+  };
+  
   const handleResize = () => {
+    const video = videoRef.current;
     const isMobile = window.innerWidth < 768;
     const desiredSrc = isMobile ? smallHeroVideo : heroVideo;
-    if (!sourceRef?.current) return;
-    if (sourceRef.current.src.endsWith(desiredSrc)) return;
-    sourceRef.current.src = desiredSrc;
-    sourceRef.current.parentElement.load();
+
+    if (!video?.source || !video?.control) return;
+    if (video.source.src.endsWith(desiredSrc)) return;
+
+    video.source.src = desiredSrc;
+    video.control.load();
   };
 
   useGSAP(() => {
@@ -25,30 +43,44 @@ const Hero = () => {
     gsap.to("#cta", {
       opacity: 1,
       y: -50,
-      duration:1,
-      delay: isMobile?2.4:0.8,
+      duration: 1,
+      delay: isMobile ? 2.4 : 0.8,
     });
   }, [heroVideo, smallHeroVideo]);
 
   React.useEffect(() => {
-    handleResize();
+    const video = videoRef.current.control;
+    if (!video) return;
+    video.addEventListener("timeupdate", loopEnd);
+
     window.addEventListener("resize", handleResize);
+    if (videoRef.current.control) video.addEventListener("timeupdate", loopEnd);
     return () => {
       window.removeEventListener("resize", handleResize);
-      if (sourceRef?.current) sourceRef.current = null;
+      video.removeEventListener("timeupdate", loopEnd);
+      if (videoRef?.current?.control || videoRef?.current?.source) {
+        videoRef.current.control = null;
+        videoRef.current.source = null;
+      }
     };
-  }, [heroVideo, smallHeroVideo]);
+  }, [videoRef]);
 
   return (
-    <section className="nav-height relative">
+    <section className="nav-height relative overflow-hidden">
       <div className="flex-center h-5/6 flex-col">
         <p className="hero-title" id="hero">
           iPhone 15 Pro
         </p>
         <div className="w-9/12 md:w-10/12">
-          <video autoPlay muted playsInline className="pointer-events-none">
+          <video
+            ref={(el) => (videoRef.current.control = el)}
+            autoPlay
+            muted
+            playsInline
+            className="pointer-events-none"
+          >
             <source
-              ref={sourceRef}
+              ref={(el) => (videoRef.current.source = el)}
               src={window.innerWidth < 768 ? smallHeroVideo : heroVideo}
               type="video/mp4"
             />
@@ -59,7 +91,7 @@ const Hero = () => {
         <a href="#highlights" className="btn">
           Buy
         </a>
-        <p className="text-xl font-normal">From 1199/month or 1999</p>
+        <p className="text-xl font-normal">From $199/month or $1999</p>
       </div>
     </section>
   );
--- src/components/HighLights.jsx
+++ src/components/HighLights.jsx
@@ -1,3 +1,49 @@
-const HighLights = () => <div>HighLights</div>;
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { rightImg, watchImg } from "../assets";
+import VideoCarousel from "./VideoCarousel";
+
+const HighLights = () => {
+  useGSAP(() => {
+    gsap.to("#title", {
+      scrollTrigger: {
+        trigger: "#title",
+      },
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".link", {
+      scrollTrigger: {
+        trigger: ".link",
+      },
+      opacity: 1,
+      stagger: 0.25,
+      y: 0,
+      delay: 0.1,
+    });
+  }, []);
+  return (
+    <section id="highlights" className="common-padding bg-zinc h-full overflow-hidden">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full items-end justify-between md:flex">
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <a className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />
+            </a>
+            <a className="link">
+              Watch the event
+              <img src={rightImg} alt="watch" className="ml-2" />
+            </a>
+          </div>
+        </div>
+        <VideoCarousel />
+      </div>
+    </section>
+  );
+};
 
 export default HighLights;
